5a85f4323d860282d332b9f354c13df3b6f08793
Jaid <jaid.jsx@gmail.com>
2019-11-20 11:19:23 +0100

    Fixed rules loading
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,41 +1,12 @@
-import {getInput, setFailed} from "@actions/core"
-import {which} from "@actions/io"
-import {exec} from "@actions/exec"
-import guessPackageManager from "guess-package-manager"
-
-async function getExecInfo() {
-  const packageManager = guessPackageManager()
-  if (packageManager === "yarn") {
-    return {
-      execPath: await which("npx", true),
-      execArgs: ["yarn", "install"],
-    }
-  }
-  if (packageManager === "pnpm") {
-    return {
-      execPath: await which("npx", true),
-      execArgs: ["pnpm", "install"],
-    }
-  }
-  return {
-    execPath: await which("npm", true),
-    execArgs: ["install"],
-  }
+const rules = {}
+const rulesRequire = require.context("./rules/", true, /index.js$/)
+for (const value of rulesRequire.keys()) {
+  const {ruleName} = value.match(/[/\\](?<ruleName>.+?)[/\\]index\.js$/).groups
+  rules[ruleName] = rulesRequire(value)
 }
 
 async function main() {
-  const nodeEnv = getInput("nodeEnv")
-  const {execPath, execArgs} = await getExecInfo()
-  const exitCode = await exec(execPath, execArgs, {
-    env: {
-      ...process.env,
-      NODE_ENV: nodeEnv,
-    },
-  })
-  if (exitCode !== 0) {
-    setFailed(`Installation failed with code ${exitCode}`)
-    return
-  }
+  console.log(`Rules: ${Object.keys(rules).join(" ")}`)
 }
 
 main()
