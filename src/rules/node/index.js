da999bad8c0759b0ee411348bc35993a8f1f9707
Jaid <jaid.jsx@gmail.com>
2019-11-26 06:25:49 +0100

    Testing
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/rules/node/index.js
+++ src/rules/node/index.js
@@ -2,6 +2,8 @@ import Rule from "src/Rule"
 import PkgFieldExact from "src/testers/PkgFieldExact"
 import FileExact from "src/testers/FileExact"
 
+import jsConfig from "./js.json"
+
 export default new class extends Rule {
 
   title = "Node"
@@ -9,7 +11,7 @@ export default new class extends Rule {
   constructor() {
     super()
     this.addTester(new PkgFieldExact("author", "Jaid <jaid.jsx@gmail.com> (https://github.com/Jaid)"))
-    this.addTester(new FileExact("jsconfig.json", require("!raw-loader!./js.json").default))
+    this.addTester(new FileExact("jsconfig.json", JSON.stringify(jsConfig, null, 2)))
   }
 
 }
