99712cea5a80322f5a12cb2282d65bb80bb85e98
Jaid <jaid.jsx@gmail.com>
2019-11-28 12:02:18 +0100

    feat: Added `jaidCore` rule
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/rules/jaidCore/index.js
@@ -0,0 +1,25 @@
+import FileHasContent from "src/testers/FileHasContent"
+import Rule from "src/Rule"
+import hasDependency from "lib/hasDependency"
+import FolderExists from "src/testers/FolderExists"
+import FileExact from "src/testers/FileExact"
+
+export default new class extends Rule {
+
+  title = "Depends on jaid-core"
+
+  /**
+   * @return {Promise<boolean>}
+   */
+  async isRelevantToRepo() {
+    return hasDependency("jaid-core")
+  }
+
+  constructor() {
+    super()
+    this.addTester(new FileExact("index.js", require("!raw-loader!./srcIndex.js").default))
+    this.addTester(new FolderExists("src/plugins"))
+    this.addTester(new FileHasContent("src/core.js"))
+  }
+
+}
---
+++ src/testers/FolderExists.js
@@ -0,0 +1,37 @@
+import path from "path"
+
+import fsp from "@absolunet/fsp"
+import Tester from "src/Tester"
+import chalk from "chalk"
+
+export default class extends Tester {
+
+  /**
+   * @type {string}
+   */
+  file = null
+
+  constructor(file) {
+    super()
+    this.file = path.resolve(file)
+    this.shortFile = file
+    this.setTitle(`Directory ${chalk.yellow(this.shortFile)} should exist`)
+  }
+
+  /**
+   * @param {import("src/index").ProjectInfo} projectInfo
+   * @return {Promise<Pick<boolean, string>>}
+   */
+  async test() {
+    const exists = await fsp.pathExists(this.file)
+    if (!exists) {
+      return `${this.shortFile} does not exist`
+    }
+    const stat = await fsp.stat(this.file)
+    if (!stat.isDirectory()) {
+      return `${this.shortFile} is not a directory`
+    }
+    return true
+  }
+
+}
