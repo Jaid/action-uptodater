924398d7fbbf5dd4c27c7306bcbe3402d60b6981
Jaid <jaid.jsx@gmail.com>
2020-04-30 04:21:43 +0200

    feat: Changed `tsconfig.json` to `tsconfigBase.json`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/rules/node/index.js
+++ src/rules/node/index.js
@@ -1,9 +1,10 @@
 import Rule from "src/Rule"
 import FileExact from "src/testers/FileExact"
+import FileExists from "src/testers/FileExists"
 import FileShouldNotExist from "src/testers/FileShouldNotExist.js"
 import PkgFieldExact from "src/testers/PkgFieldExact"
 
-import tsConfig from "./ts.json"
+import tsConfigBase from "./tsconfigBase.json"
 
 export default new class extends Rule {
 
@@ -12,7 +13,8 @@ export default new class extends Rule {
   init() {
     this.addTester(new PkgFieldExact("author", "Jaid <jaid.jsx@gmail.com> (https://github.com/Jaid)"))
     this.addTester(new FileShouldNotExist("index.js"))
-    this.addTester(new FileExact("tsconfig.json", JSON.stringify(tsConfig, null, 2)))
+    this.addTester(new FileExact("tsconfigBase.json", JSON.stringify(tsConfigBase, null, 2)))
+    this.addTester(new FileExists("tsconfig.json"))
     this.addTester(new FileShouldNotExist("jsconfig.json"))
   }
 
