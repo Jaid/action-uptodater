daa333012b3bb86bab06f4ed69f8be68fd4ae7a9
Jaid <jaid.jsx@gmail.com>
2020-08-24 23:47:13 +0200

    fix: Fixed variable reference
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/rules/node/index.js
+++ src/rules/node/index.js
@@ -1,5 +1,4 @@
 import Rule from "src/Rule"
-import FileExact from "src/testers/FileExact"
 import FileExists from "src/testers/FileExists"
 import FileShouldNotExist from "src/testers/FileShouldNotExist.js"
 import PkgFieldExact from "src/testers/PkgFieldExact"
@@ -11,7 +10,6 @@ export default new class extends Rule {
   init() {
     this.addTester(new PkgFieldExact("author", "Jaid <jaid.jsx@gmail.com> (https://github.com/Jaid)"))
     this.addTester(new FileShouldNotExist("index.js"))
-    this.addTester(new FileExact("tsconfigBase.json", JSON.stringify(tsConfigBase, null, 2)))
     this.addTester(new FileExists("tsconfig.json"))
     this.addTester(new FileShouldNotExist("jsconfig.json"))
     this.addTester(new FileShouldNotExist("tsconfigBase.json"))
