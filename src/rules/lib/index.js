1a32a82f7f657e68ced40bf571c741203931f6d8
Jaid <jaid.jsx@gmail.com>
2021-05-24 01:42:06 +0200

    Testing changes

--- src/rules/lib/index.js
+++ src/rules/lib/index.js
@@ -11,27 +11,19 @@ export default new class extends Rule {
    * @return {Promise<boolean>}
    */
   async isRelevantToRepo() {
-    console.log(JSON.stringify(this.pkg))
-    console.log("step1")
-    if (!this.hasDependency("webpack-config-jaid")) {
-      return false
-    }
-    console.log("step2")
-    if (!this.pkg.webpackConfigJaid) {
-      return false
-    }
-    console.log("step3")
     // Rule “lib” should run for packages depending on webpack-config-jaid
     // webpack-config-jaid also depends on itself, but it imports its own source instead of a Node dependency
     // So we need to add it as an exception here
-    if (this.pkg.name === "webpack-config-jaid") {
-      return true
+    const pkgIsWebpackConfigJaid = this.pkg.name === "webpack-config-jaid"
+    if (!this.hasDependency("webpack-config-jaid") && !pkgIsWebpackConfigJaid) {
+      return false
     }
-    console.log("step4")
+    if (!this.pkg.webpackConfigJaid) {
+      return false
+    }
     if (this.pkg.webpackConfigJaid.endsWith("Lib") || this.pkg.webpackConfigJaid.endsWith("Class")) {
       return true
     }
-    console.log("step5")
     return false
   }
 
